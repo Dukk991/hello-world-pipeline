09a87dd875b4e459da8321745148d9f951db5d6d
Valtteri K.A.P <144036143+Dukk991@users.noreply.github.com>
2024-10-11 06:28:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send("Hello World from Pipeline");
 });
 
+app.get('/', (req, res) => {
+  res.send("Update_1");
+});
+
 app.listen(port, () => {
   console.log("Running");
 });
